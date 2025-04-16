1b05077b82245a24a912a75e5a087ba74f18c7c5
shibili888 <shibilishanu888@gmail.com>
2025-04-16 10:58:12 +0530

    pwa testing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import VitePWA from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
@@ -8,7 +8,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate', // auto-updates the service worker
       workbox: {
-        runtimeCaching: [ 
+        runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com/,
             handler: 'CacheFirst',
@@ -29,16 +29,6 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         icons: [
-          {
-            src: '/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
         ],
       },
     }),
